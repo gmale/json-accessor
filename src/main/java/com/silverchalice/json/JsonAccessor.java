5eea82cef24db9859a08141183b72df95d777b51
Kevin Gorham <kevin@silverchalice.com>
2013-06-05 10:46:38 -0600

    clarified the types that should be passed to the API. Previously, api methods accepted an Object but it was not clear that this could be either a JSONObject or JSONArray nor was it clear which implementation of those classes was expected. Also, it seemed like maybe a string would be acceptable. So I created helper methods that clarify exactly what type of object the API expects.

--- src/main/java/com/silverchalice/json/JsonAccessor.java
+++ src/main/java/com/silverchalice/json/JsonAccessor.java
@@ -6,8 +6,54 @@ import org.json.JSONObject;
 public class JsonAccessor {
 
 	public JsonAccessor() {
+		super();
 	}
 
+	//--------------------------------------------------------------------------
+	//
+	// Public API
+	//
+	//--------------------------------------------------------------------------	
+	
+	/**  @see #getKeyPathRecursively(String, Object) */
+	public Object getKeyPath(String keyPath, JSONObject jsonObject) {
+		return getKeyPath(keyPath, jsonObject);
+	}
+	/**  @see #getKeyPathRecursively(String, Object) */
+	public Object getKeyPath(String keyPath, JSONArray jsonArray) {
+		return getKeyPath(keyPath, jsonArray);
+	}
+	/**  @see #getKeyPathRecursively(String, Object) */
+	public Object getKeyPath(String keyPath, JSONObject json, Object defaultValue) {
+		Object result = getKeyPathRecursively(keyPath, json);
+		return result == null ? defaultValue : result;
+	}
+	/**  @see #getKeyPathRecursively(String, Object) */
+	public Object getKeyPath(String keyPath, JSONArray json, Object defaultValue) {
+		Object result = getKeyPathRecursively(keyPath, json);
+		return result == null ? defaultValue : result;
+	}
+	/**  @see #getKeyPathRecursively(String, Object) */
+	@SuppressWarnings("unchecked")
+	public <T> T getKeyPath(String keyPath, JSONObject json, Class<T> type) {
+		Object value = getKeyPathRecursively(keyPath, json);
+		if(type.isInstance(value)) return (T) value;
+		else return coerceValue(value, type);
+	}
+	/**  @see #getKeyPathRecursively(String, Object) */
+	@SuppressWarnings("unchecked")
+	public <T> T getKeyPath(String keyPath, JSONArray json, Class<T> type) {
+		Object value = getKeyPathRecursively(keyPath, json);
+		if(type.isInstance(value)) return (T) value;
+		else return coerceValue(value, type);
+	}
+	
+	//--------------------------------------------------------------------------
+	//
+	// Implementation
+	//
+	//--------------------------------------------------------------------------	
+	
 	/**
 	 * Recursive function to get a key string from a json object. Allows key
 	 * strings to be expressed as:<br/>
@@ -18,30 +64,18 @@ public class JsonAccessor {
 	 * <br/>
 	 * <code>newsStories[0].assets[0].links[3].url</code>
 	 */
-	public Object getKeyPath(String keyPath, Object jsonObjectOrArray) {
+	protected Object getKeyPathRecursively(String keyPath, Object jsonObjectOrArray) {
 		if(keyPath == null || jsonObjectOrArray == null) return null;
 		//chop off the first part of the key, treating multiple delimiters as one -kg
 		String[] keys = keyPath.split("[.\\[\\]]+", 2);
 		//if no more keys, return final result -kg
 		if(keys.length == 1) return accessKey(keys[0], jsonObjectOrArray);
 		//else, recurse -kg
-		return getKeyPath(keys[1], accessKey(keys[0], jsonObjectOrArray));
+		return getKeyPathRecursively(keys[1], accessKey(keys[0], jsonObjectOrArray));
 	}
 
-	public Object getKeyPath(String keyPath, Object jsonObjectOrArray, Object defaultValue) {
-		Object result = getKeyPath(keyPath, jsonObjectOrArray);
-		return result == null ? defaultValue : result;
-	}
-	
 	@SuppressWarnings("unchecked")
-	public <T> T getKeyPath(String keyPath, Object jsonObjectOrArray, Class<T> type) {
-		Object value = getKeyPath(keyPath, jsonObjectOrArray);
-		if(type.isInstance(value)) return (T) value;
-		else return coerceValue(value, type);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public <T> T coerceValue(Object value, Class<T> type) {
+	protected <T> T coerceValue(Object value, Class<T> type) {
 		if(value == null) return (T) null;
 		/* by definition, 'value' is not an instance of 'type' */
 		try {
@@ -63,7 +97,7 @@ public class JsonAccessor {
 	 * whether <code>nextJson</code> is a JSONObject or JSONArray. When nextJson
 	 * is a JSONArray, it's expected that the key is a digit.
 	 */
-	private Object accessKey(String key, Object nextJson) {
+	protected Object accessKey(String key, Object nextJson) {
 		if(key == null || key.length() == 0) {
 			return nextJson;
 		} else if(nextJson instanceof JSONArray) {
@@ -75,7 +109,7 @@ public class JsonAccessor {
 		}
 	}
 
-	public Object getArrayIndex(String index, JSONArray nextJson) {
+	protected Object getArrayIndex(String index, JSONArray nextJson) {
 		if(index.trim().matches("\\d+")) { 
 			int i = Integer.parseInt(index);
 			if(i > nextJson.length()) throw new ArrayIndexOutOfBoundsException("ERROR [JsonAccessor] : Invalid index: " + i + "\n" + nextJson);
